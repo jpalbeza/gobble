f0634a4d78e791473975ffa44ad6c9f912366566
JP Albeza <johnpaul.albeza@gmail.com>
2016-04-23 23:42:48 +1000

    Initial stab at implementation. Works well for open frames. Includes some testing.

--- src/gobble/core.clj
+++ src/gobble/core.clj
@@ -1,5 +1,68 @@
 (ns gobble.core)
 
+(def all-pins 10)
+(def frame-size 3)
+(def empty-score "")
+(def num-of-frames 10)
+(def max-frame-size 4)
+
 (defn new-scorecard
   "Create new empty score card."
-  [] '())
+  [] [])
+
+(defn to-balls "Map frame scores to the raw score of each ball."
+  [card]
+  (let [balls (->> (rest card)
+                   (partition 2 frame-size)
+                   (flatten)
+                   (filter #(not= "" %)))]
+    (->> (count balls)
+         range
+         (map #(let [current (nth balls %)]
+                (cond (= \X current) all-pins
+                      (= \/ current) (- all-pins (nth balls (dec %)))
+                      :else current))))))
+
+(defn resolve-frame-score
+  "Resolve a frames score"
+  [[fscore b1 b2 & rest-of-card]]
+  (if (and (number? b1) (number? b2))
+    (+ b1 b2)                                                                               ;open frame
+    (let [next-balls (to-balls (partition 2 frame-size (rest rest-of-card)))
+          num-of-next-balls (count next-balls)]
+      (cond (and (= b1 \X) (>= num-of-next-balls 2)) (apply + all-pins (take 2 next-balls)) ;strike
+            (and (= b2 \/) (>= num-of-next-balls 1)) (+ all-pins (first next-balls))        ;spare
+            :else ""))))                                                                    ;cannot be resolved yet, need more balls
+
+(defn resolve-card-score
+  "Resolve blank scores as much as possible."
+  [card]
+  (let [card-size (count card)]
+    (->> (- card-size frame-size)
+         (iterate #(- % frame-size))
+         (take-while #(and (>= % 0) ((complement number?) (nth card %))))
+         (map #(list % (resolve-frame-score (drop % card))))
+         (reduce (fn [new-card [score-pos score]]
+                   (update-in new-card [score-pos] #(identity %2) score))
+                 card)
+         )))
+
+(defn is-valid? [card & balls] true)                        ;TO DO
+
+
+(defn on-last-frame?
+  "Evaluates if we have all the frames already."
+  [card] (= (quot (count card) frame-size) num-of-frames))
+
+
+(defn score-frame
+  "Calculate score for frame. When the operation is valid, returns an updated scorecard.
+   Returns nil otherwise. "
+  ([card & balls] (let [new-card (resolve-card-score (into card (apply list empty-score balls)))]
+                        (if (on-last-frame? new-card)
+                          (->> (drop-last new-card)         ;take out the fill-ball entry
+                               (partition 1 frame-size)
+                               (reduce +)
+                               (into new-card))
+                          new-card)))
+  )
--- test/gobble/core_test.clj
+++ test/gobble/core_test.clj
@@ -4,6 +4,28 @@
 
 (deftest test-new-scorecard
   (testing "New scorecard"
-    (is (let [initial-scorecard (new-scorecard)]
-          (and (empty? initial-scorecard)
-               (seq? initial-scorecard))))))
+    (is (let [card (new-scorecard)]
+          (and (empty? card)
+               (vector? card))))))
+
+(def empty-card [])
+(def one-frame-card [3 1 2])
+;(def nine-frame-card (vec (take 27 (cycle [3 1 2]))))
+
+(deftest test-open-frame-on-empty
+  (testing "open frame score on empty card"
+    (is (= '(9 4 5) (score-frame empty-card 4 5)))))
+
+(deftest test-open-frame-on-non-empty
+  (testing "open frame score on a card with 1 open frame"
+    (is (= '(3 1 2 3 1 2) (score-frame one-frame-card 1 2)))))
+
+(deftest test-to-rolls
+  (testing "test transforming frames to rolls"
+    (is (and (= [all-pins] (to-rolls '("" \X )))
+             (= [5 5] (to-rolls '("" 5 \/ )))))))
+
+;(deftest test-last-open-frame
+;  (testing "last open frame"
+;    (is (= (into nine-frame-card [3 1 2 30]) (score-frame nine-frame-card 1 2)))))
+
