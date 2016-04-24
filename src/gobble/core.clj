ec0d69b9c01c385a5719ae0005d033c6eae472fe
JP Albeza <johnpaul.albeza@gmail.com>
2016-04-24 20:22:31 +1000

    New data structure using map to allow separation of frame data from frame scores. Also add in placeholder for running-total and final total.

--- src/gobble/core.clj
+++ src/gobble/core.clj
@@ -6,46 +6,40 @@
 (def num-of-frames 10)
 (def max-frame-size 4)
 
+
 (defn new-scorecard
   "Create new empty score card."
-  [] [])
+  [] {:frames [], :per-frame-score [], :running-total [], :final-total "" })
 
-(defn to-balls "Map frame scores to the raw score of each ball."
-  [card]
-  (let [balls (->> (rest card)
-                   (partition 2 frame-size)
-                   (flatten)
-                   (filter #(not= "" %)))]
-    (->> (count balls)
-         range
-         (map #(let [current (nth balls %)]
-                (cond (= \X current) all-pins
-                      (= \/ current) (- all-pins (nth balls (dec %)))
-                      :else current))))))
+
+(defn to-ball-scores "Map frame scores to the raw score of each ball."
+  [balls]
+  (->> balls
+       (map #(let [current (nth balls %)]
+              (cond (= \X current) all-pins
+                    (= \/ current) (- all-pins (nth balls (dec %)))
+                    :else current)))))
+
 
 (defn resolve-frame-score
   "Resolve a frames score"
-  [[fscore b1 b2 & rest-of-card]]
+  [[b1 b2 & rest-of-frames]]
   (if (and (number? b1) (number? b2))
     (+ b1 b2)                                                                               ;open frame
-    (let [next-balls (to-balls (partition 2 frame-size (rest rest-of-card)))
+    (let [next-balls (to-ball-scores rest-of-frames)
           num-of-next-balls (count next-balls)]
       (cond (and (= b1 \X) (>= num-of-next-balls 2)) (apply + all-pins (take 2 next-balls)) ;strike
             (and (= b2 \/) (>= num-of-next-balls 1)) (+ all-pins (first next-balls))        ;spare
             :else ""))))                                                                    ;cannot be resolved yet, need more balls
 
+
 (defn resolve-card-score
   "Resolve blank scores as much as possible."
-  [card]
-  (let [card-size (count card)]
-    (->> (- card-size frame-size)
-         (iterate #(- % frame-size))
-         (take-while #(and (>= % 0) ((complement number?) (nth card %))))
-         (map #(list % (resolve-frame-score (drop % card))))
-         (reduce (fn [new-card [score-pos score]]
-                   (update-in new-card [score-pos] #(identity %2) score))
-                 card)
-         )))
+  [{:keys [frames per-frame-score running-total final-total] :as card}]
+  (->> (range (count per-frame-score) (count frames))
+       (map #((comp resolve-frame-score flatten drop) % frames))
+       (filter number?)
+       (reduce (fn [new-card score] (update-in new-card [:per-frame-score] conj score)) card)))
 
 (defn is-valid? [card & balls] true)                        ;TO DO
 
@@ -58,7 +52,7 @@
 (defn score-frame
   "Calculate score for frame. When the operation is valid, returns an updated scorecard.
    Returns nil otherwise. "
-  ([card & balls] (let [new-card (resolve-card-score (into card (apply list empty-score balls)))]
+  ([card & balls] (let [new-card (resolve-card-score (update-in card [:frames] #(conj %1 %2) balls))]
                         (if (on-last-frame? new-card)
                           (->> (drop-last new-card)         ;take out the fill-ball entry
                                (partition 1 frame-size)
--- test/gobble/core_test.clj
+++ test/gobble/core_test.clj
@@ -4,21 +4,34 @@
 
 (deftest test-new-scorecard
   (testing "New scorecard"
-    (is (let [card (new-scorecard)]
-          (and (empty? card)
-               (vector? card))))))
+    (is (= (new-scorecard)  {:frames [], :per-frame-score [], :running-total [], :final-total "" }))))
 
-(def empty-card [])
-(def one-frame-card [3 1 2])
-;(def nine-frame-card (vec (take 27 (cycle [3 1 2]))))
+(def empty-card {:frames [], :per-frame-score [], :running-total [], :final-total "" })
+(def one-frame-card (update-in empty-card [:frames] #(conj %1 %2) '(2 3)))
+(def nine-frame-card (update-in empty-card [:frames] #(into %1 %2) (repeat 9 '(2 3))))
 
 (deftest test-open-frame-on-empty
   (testing "open frame score on empty card"
-    (is (= '(9 4 5) (score-frame empty-card 4 5)))))
+    (let [updated-card (score-frame empty-card 4 5)]
+      (is (= ['(4 5)] (get-in updated-card [:frames]))))))
+
+(deftest test-open-frame-on-empty-check-score
+  (testing "open frame score on empty card, check per-frame-score"
+    (let [updated-card (score-frame empty-card 4 5)]
+      (is (= {} updated-card)))))
+
+(deftest test-open-frame-on-non-empty-checkscore
+  (testing "open frame score on empty card, check per-frame-score"
+    (let [updated-card (score-frame one-frame-card 4 5)]
+      (is (= {} updated-card)))))
 
 (deftest test-open-frame-on-non-empty
   (testing "open frame score on a card with 1 open frame"
-    (is (= '(3 1 2 3 1 2) (score-frame one-frame-card 1 2)))))
+    (is (= ['(2 3) '(4 5)] (get-in (score-frame one-frame-card 4 5) [:frames])))))
+
+(deftest test-almost-full-scorecard
+  (testing "open frame score on a card with 1 open frame"
+    (is (= (into [] (concat (repeat 9 '(2 3)) '((6 7 8)))) (get-in (score-frame nine-frame-card 6 7 8) [:frames])))))
 
 (deftest test-to-rolls
   (testing "test transforming frames to rolls"
