fe031471ac79fc0bb9dbe35d15c357873281feed
JP Albeza <johnpaul.albeza@gmail.com>
2016-04-25 21:11:52 +1000

    Beautify tests and comments. Support gutter balls.

--- src/gobble/core.clj
+++ src/gobble/core.clj
@@ -17,6 +17,19 @@
   (throw (RuntimeException. "Total score is more than available pins.")))
 
 (defn is-valid
+  "Validate the input balls. Invalid scenarios:
+  1. No balls
+  2. Trying to add a frame to a scorecard that is already full.
+  3. Those that do not fit these valid scenarios:
+      - For non-last frames
+         - 1 ball which is just a strike
+         - 2 balls which are either 2 numbers that add up to less than 10, a number less than 10 and a spare symbol, or a strike and an empty score
+      - For last frames
+         - 2 balls which are 2 numbers that add up to less than 10
+         - 3 balls which are either:
+            - a number less than 10 plus a spare plus another number less than 10
+            - a number less than 10 plus a spare and a strike
+            - 3 strikes"
   ([frame-pos] (throw-invalid-ball-count-msg))
   ([frame-pos b1] (cond (>= frame-pos num-of-frames) (throw-invalid-ball-count-msg)
                         ((complement =) b1 \X) (throw-invalid-ball-score-msg)))
@@ -27,13 +40,11 @@
                            (is-valid frame-pos b1 b2 empty-score) ;validate as last frame
 
                            :else (let [balls-str (clojure.string/join "" [b1 b2])]
-                                   (cond  (not (re-matches #"[1-9]{2}|X|[1-9]/" balls-str))
+                                   (cond  (not (re-matches #"[0-9]{2}|X|[0-9]/" balls-str))
                                           (throw-invalid-ball-score-msg)
 
-                                          (and (re-matches #"[1-9]{2}" balls-str) (<= all-pins (+ b1 b2)))
+                                          (and (re-matches #"[0-9]{2}" balls-str) (<= all-pins (+ b1 b2)))
                                           (throw-invalid-ball-score-range-msg)))))
-
-
   ([frame-pos b1 b2 b3] (cond (>= frame-pos num-of-frames)
                               (throw-full-card-msg)             ;cannot add more frames to a full set
 
@@ -41,17 +52,20 @@
                               (throw-invalid-ball-count-msg)    ;3 elements are only allowed in last frame
 
                               :else (let [balls-str (clojure.string/join "" [b1 b2 b3])]
-                                      (cond  (not (re-matches #"[1-9]{2}|XXX|[1-9]/[1-9]|[1-9]/X" balls-str))
+                                      (cond  (not (re-matches #"[0-9]{2}|XXX|[0-9]/[0-9]|[0-9]/X" balls-str))
                                              (throw-invalid-ball-score-msg)
 
-                                             (and (re-matches #"[1-9]{2}" balls-str) (<= all-pins (+ b1 b2)))
+                                             (and (re-matches #"[0-9]{2}" balls-str) (<= all-pins (+ b1 b2)))
                                              (throw-invalid-ball-score-range-msg)))))
   ([frame-pos b1 b2 b3 b4 & more] (throw-invalid-ball-count-msg)))
 
-(defn new-scorecard "Create new empty score card."
-  [] {:frames [] :per-frame-scores [] :running-total [] :final-total empty-score })
+(defn new-scorecard
+  "Create new empty score card."
+  []
+  {:frames [] :per-frame-scores [] :running-total [] :final-total empty-score })
 
-(defn to-rolls "Map frame scores to the raw score of each ball."
+(defn to-rolls
+  "Map frame scores to the raw score of each ball."
   [balls]
   (let [rolls (replace {\X all-pins} balls)]
     (->> (range (count rolls))
@@ -60,7 +74,8 @@
                   (- all-pins (nth rolls (dec %)))
                   current))))))
 
-(defn resolve-frame-score "Resolve a frames score"
+(defn resolve-frame-score
+  "Try to resolve the scores for unresolved frames."
   [[b1 & rest-of-frames]]
   (let [rolls-after-b1 (remove #(= empty-score %) rest-of-frames) ;eliminate empty-scores as they are not considered as rolls
         b2 (first rolls-after-b1)
@@ -71,8 +86,8 @@
           :else empty-score)))                              ;cannot be resolved yet, need more rolls
 
 (defn score-frame
-  "Calculate score for frame. When the operation is valid, returns an updated scorecard.
-   Returns nil otherwise. "
+  "Calculate score for frame. When the balls are valid, returns an updated scorecard.
+  Throws an appropriate exception otherwise."
   [{:keys [frames per-frame-scores running-total final-total] :as card}
    & balls]
   (let [new-frames (conj frames balls)
@@ -82,7 +97,10 @@
         new-running-total (reduce #(conj %1 ((fnil + 0) (last %1) %2) )
                                   running-total
                                   additional-frame-scores)]
+
+    ;only validate the balls. Assume scorecard is only manipulated by new-scorecard or score-frame functions here
     (apply is-valid (count frames) balls)
+
     (into card {:frames            new-frames
                 :per-frame-scores  (into per-frame-scores additional-frame-scores)
                 :running-total     new-running-total
--- test/gobble/core_test.clj
+++ test/gobble/core_test.clj
@@ -3,7 +3,7 @@
             [gobble.core :refer :all]))
 
 (deftest test-new-scorecard
-  (testing "New scorecard"
+  (testing "Test new scorecard."
     (is (= (new-scorecard)  {:frames [] :per-frame-scores [] :running-total [] :final-total "" }))))
 
 (def empty-card {:frames [] :per-frame-scores [] :running-total [] :final-total "" })
@@ -17,7 +17,7 @@
 (def tsi #"Total score is more")
 
 (deftest test-invalid-frames
-  (testing "Verify that appropriate exception is thrown for invalid input"
+  (testing "Verify that appropriate exception is thrown for invalid input."
     (is (thrown-with-msg? RuntimeException caf (score-frames (repeat 11 '(1 2)))))
     (is (thrown-with-msg? RuntimeException caf (score-frames (concat (repeat 10 '(1 2)) '((\X \X \X))))))
 
@@ -34,55 +34,79 @@
 
     (is (thrown-with-msg? RuntimeException tsi (score-frames '((5 5)))))))
 
-(deftest test-open-frame-on-empty
-  (testing "open frame score on empty card"
+(deftest test-score-card
+  (testing "Test happy paths on the score card."
+    (is (= (new-scorecard)
+           {:frames [] :per-frame-scores [] :running-total [] :final-total "" })
+        "Wrong empty scorecard.")
+
     (is (= {:frames ['(4 5)]
             :per-frame-scores [9]
             :running-total [9]
             :final-total "" }
            (score-frame empty-card 4 5))
-        "something happened")))
-
-(deftest test-almost-full-scorecard
-  (testing "open frame score on a card with 1 open frame"
+        "Wrong card when adding open frame to empty card.")
     (is (= {:frames ['(2 3) '(4 5)]
             :per-frame-scores [5 9]
             :running-total [5 14]
             :final-total "" }
-           (score-frame one-frame-card 4 5)))))
-
-(deftest test-first-spare
-  (testing "open frame score on a card with 1 open frame"
+           (score-frame one-frame-card 4 5))
+        "Wrong card when adding open frame to card with 1 frame.")
     (is (= {:frames ['(4 \/)]
             :per-frame-scores []
             :running-total []
             :final-total "" }
-           (score-frame empty-card 4 \/)))))
-
-(deftest test-first-spare-followed-by-open
-  (testing "open frame score on a card with 1 open frame"
+           (score-frame empty-card 4 \/))
+        "Wrong card when adding first spare.")
     (is (= {:frames ['(4 \/) '(4 5)]
             :per-frame-scores [14 9]
             :running-total [14 23]
             :final-total "" }
-            (reduce #(apply score-frame %1 %2) empty-card ['(4 \/) '(4 5)]) ))))
-
-
-(deftest test-opening-turkey
-  (testing "three strikes in the beginning"
+           (score-frames ['(4 \/) '(4 5)]))
+        "Wrong card when adding open after first spare.")
     (is (= {:frames (into [] (repeat 3 '(\X "")))
             :per-frame-scores [30]
             :running-total [30]
             :final-total "" }
-           (reduce #(apply score-frame %1 %2) empty-card (into [] (repeat 3 '(\X ""))))))))
-
-(deftest test-perfect-game
-  (testing "perfect game"
+           (reduce #(apply score-frame %1 %2) empty-card (into [] (repeat 3 '(\X "")))))
+        "Wrong card for turkey in the beginning.")
     (is (= {:frames (into [] (concat (repeat 9 '(\X "")) '((\X \X \X))))
             :per-frame-scores (into [] (repeat 10 30))
             :running-total (into [] (take 10 (iterate #(+ 30 %) 30)))
             :final-total 300 }
-           (reduce #(apply score-frame %1 %2) empty-card (concat (repeat 9 '(\X "")) '((\X \X \X))))))))
+           (score-frames (concat (repeat 9 '(\X "")) '((\X \X \X)))))
+        "Wrong card for perfect game.")
+    (is (= {:frames (into [] (concat (repeat 9 '(0 \/)) '((0 \/ 0))))
+            :per-frame-scores (into [] (repeat 10 10))
+            :running-total (into [] (take 10 (iterate #(+ 10 %) 10)))
+            :final-total 100 }
+           (score-frames (concat (repeat 9 '(0 \/)) '((0 \/ 0)))))
+        "Wrong card for gutter spares.")
+    (is (= {:frames (into [] (concat (repeat 9 '(9 \/)) '((9 \/ 9))))
+            :per-frame-scores (into [] (repeat 10 19))
+            :running-total (into [] (take 10 (iterate #(+ 19 %) 19)))
+            :final-total 190 }
+           (score-frames (concat (repeat 9 '(9 \/)) '((9 \/ 9)))))
+        "Wrong card for gutter spares.")
+    (is (= {:frames (into [] (concat (repeat 9 '(9 \/)) '((9 \/ \X))))
+            :per-frame-scores (into [] (concat (repeat 9 19) '(20)))
+            :running-total (into [] (concat (take 9 (iterate #(+ 19 %) 19)) '(191)))
+            :final-total 191 }
+           (score-frames (concat (repeat 9 '(9 \/)) '((9 \/ \X)))))
+        "Wrong card for gutter spares and strike on last roll.")
+    (is (= {:frames (into [] (repeat 10 '(5 4)))
+            :per-frame-scores (into [] (repeat 10 9))
+            :running-total (into [] (take 10 (iterate #(+ 9 %) 9)))
+            :final-total 90 }
+           (score-frames (repeat 10 '(5 4))))
+        "Wrong card for stubborn pin.")
+    (is (= {:frames ['(0 9) '(0 \/) '(\X)' (0 9) '(0 \/) '(\X) '(0 9) '(0 \/) '(\X) '(\X \X \X)]
+            :per-frame-scores [9 20 19 9 20 19 9 20 30 30]
+            :running-total [9 29 48 57 77 96 105 125 155 185]
+            :final-total 185 }
+           (score-frames ['(0 9) '(0 \/) '(\X)' (0 9) '(0 \/) '(\X) '(0 9) '(0 \/) '(\X) '(\X \X \X)]))
+        "Wrong card for beginners luck.")
+    ))
 
 (deftest test-to-rolls
   (testing "test transforming frames to rolls"
