669b3b85a8c5b71c3914091d0efb24fbd52e6b39
JP Albeza <johnpaul.albeza@gmail.com>
2016-05-02 06:56:57 +1000

    Little fixes to README and project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gobble "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Gobble: A library for computing score cards in game of ten-pin bowling."
+  :url "https://github.com/jpalbeza/gobble"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
